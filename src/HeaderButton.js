3bd4c96bd7c2d61c50149c065df7d326d33ba27a
MiPeSa <bgu109@myy.haaga-helia.fi>
2022-11-07 14:00:35 +0200

    buttontheme tehtävä valmis

--- src/HeaderButton.js
+++ src/HeaderButton.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from './ThemeContext';
 
 function HeaderButton() {
+  const theme = useContext(ThemeContext);
   return (
     <div>
-      <button>Press me</button>
+      <button style={{color: theme.color, background: theme.backgroundColor}}>Press me</button>
     </div>
   );
 }
